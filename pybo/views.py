e0517120dbbedf30cdf0bea644ff2e72a38c9fa9
sunwoo07 <psw8557@gmail.com>
2023-11-19 03:02:36 +0900

    3-02

--- pybo/views.py
+++ pybo/views.py
@@ -3,9 +3,14 @@ from django.utils import timezone
 from .models import Question, Answer
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
